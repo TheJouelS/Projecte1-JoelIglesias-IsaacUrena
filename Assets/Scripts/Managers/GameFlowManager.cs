b2ad646da9ba06d2b9b2bcea5cc5385068bee4cb
Joel Iglesias <joeltorminecraft@gmail.com>
2024-05-15 14:16:22 +0200

    FMS aplicado en el menú de pausa, inicio y reinicio (fin)
    He aplicado el FMS para el Manager del tiempo y establecer los diferentes estados de la partida, pausa, inicio y fin, además de uno extra al llegar al final del juego.

---
+++ Assets/Scripts/Managers/GameFlowManager.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameFlowManager : MonoBehaviour
+{
+    public KeyCode keyToStart, keyToPause, keyToRestart;
+    private EGameState currentGameState = EGameState.START;
+    private bool maxLevelReached = false, stopExecuting = false;
+    private enum EGameState
+    {
+        START,
+        PLAYING,
+        PAUSED,
+        MAX_LEVEL_REACHED,
+        RESTART
+    }
+
+    private void Start()
+    {
+        EnterCurrentState();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(keyToStart) && (currentGameState == EGameState.START || currentGameState == EGameState.PAUSED))
+            ChangeGameState(EGameState.PLAYING);
+
+        if (Input.GetKeyDown(keyToPause) && currentGameState == EGameState.PLAYING)
+            ChangeGameState(EGameState.PAUSED);
+
+        if(!stopExecuting)
+            if (PlayerLevel.GetPlayerLevel() == PlayerLevel.GetMaxLevel() && PlayerScore.GetScore() == PlayerLevel.GetNextGoal())
+                ChangeGameState(EGameState.MAX_LEVEL_REACHED);
+
+        if (currentGameState == EGameState.MAX_LEVEL_REACHED)
+        {
+            if(Input.GetKeyDown(keyToRestart))
+                ChangeGameState(EGameState.RESTART);
+            else if (Input.GetKeyDown(keyToStart))
+                ChangeGameState(EGameState.PLAYING);
+        }
+
+        if (currentGameState == EGameState.PAUSED && maxLevelReached)
+        {
+            if (Input.GetKeyDown(keyToRestart))
+                ChangeGameState(EGameState.RESTART);
+        }
+    }
+
+    private void ChangeGameState(EGameState newState)
+    {
+        if (currentGameState == newState)
+            return;
+        Debug.Log(newState.ToString());
+        ExitCurrentState();
+        currentGameState = newState;
+        EnterCurrentState();
+    }
+
+    private void EnterCurrentState()
+    {
+        switch (currentGameState)
+        {
+            case EGameState.START:
+                Time.timeScale = 0.0f;
+                break;
+            case EGameState.PLAYING:
+                break;
+            case EGameState.PAUSED:
+                Time.timeScale = 0.0f;
+                break;
+            case EGameState.MAX_LEVEL_REACHED:
+                //PANTALLA FINAL CON RESULTADOS Y TIEMPO DE PARTIDA (UI/MENÚ) --> CANVAS (para la D3) / Lo mismo para es estado de PAUSE, pero con menos elementos en pantalla
+                Time.timeScale = 0.0f;
+                maxLevelReached = true;
+                stopExecuting = true;
+                break;
+            case EGameState.RESTART:
+                Time.timeScale = 0.0f;
+                ResetGame();
+                break;
+        }
+    }
+
+    private void ExitCurrentState()
+    {
+        switch (currentGameState)
+        {
+            case EGameState.START:
+                Time.timeScale = 1.0f;
+                break;
+            case EGameState.PLAYING:
+                break;
+            case EGameState.PAUSED:
+                Time.timeScale = 1.0f;
+                break;
+            case EGameState.MAX_LEVEL_REACHED:
+                Time.timeScale = 1.0f;
+                break;
+            case EGameState.RESTART:
+                Time.timeScale = 1.0f;
+                break;
+        }
+    }
+
+    private void ResetGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
