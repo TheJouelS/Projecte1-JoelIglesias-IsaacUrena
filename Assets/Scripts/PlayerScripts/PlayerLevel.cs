12fefa3a12a586c35c2a359f1c277a5b72deab6f
TheJouelS <joeltorminecraft@gmail.com>
2024-05-15 19:31:20 +0200

    Ya sé cómo aplicar Singletone, falta hacerlo
    Purebas irrelevantes

--- Assets/Scripts/PlayerScripts/PlayerLevel.cs
+++ Assets/Scripts/PlayerScripts/PlayerLevel.cs
@@ -9,10 +9,11 @@ public class PlayerLevel : MonoBehaviour
     private static int playerLevel = 1;
     private static int maxLevel = 18;
     private static int nextGoal;
-    private static int scoringMargin = 5; //USAR PARA PONER NIVEL!!!!!!!!!!!!!!!!!! en nextGoal (en vez del 1 hardcodeado que hay)
+    public int scoringMargin = 5; //USAR PARA PONER NIVEL!!!!!!!!!!!!!!!!!! en nextGoal (en vez del 1 hardcodeado que hay)
 
     private void Start()
     {
+        playerLevel = 1;
         LevelUp(true);
     }
 
